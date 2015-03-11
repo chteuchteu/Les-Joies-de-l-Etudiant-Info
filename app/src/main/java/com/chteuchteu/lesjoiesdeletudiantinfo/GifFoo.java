ea1a6e11f206f1367cddd4589ababf165a0c9c11
Quentin S <chteuchteu@gmail.com>
2015-03-11 22:38:00 +0100

    Move notifications PollTask to module

--- app/src/main/java/com/chteuchteu/lesjoiesdeletudiantinfo/GifFoo.java
+++ app/src/main/java/com/chteuchteu/lesjoiesdeletudiantinfo/GifFoo.java
@@ -6,12 +6,14 @@ import com.chteuchteu.gifapplicationlibrary.i.IDataSourceParser;
 import com.chteuchteu.gifapplicationlibrary.obj.Gif;
 import com.chteuchteu.gifapplicationlibrary.obj.GifApplicationBundle;
 import com.chteuchteu.lesjoiesdeletudiantinfo.hlpr.RSSReader;
+import com.chteuchteu.lesjoiesdeletudiantinfo.ui.Activity_Main;
 
 import java.util.List;
 
 public class GifFoo {
 	public static GifApplicationBundle getApplicationBundle(Context context) {
 		return new GifApplicationBundle(
+				context.getString(R.string.app_name),
 				"http://lesjoiesdeletudiantinfo.com/feed/",
 				new IDataSourceParser() {
 					@Override
@@ -20,7 +22,8 @@ public class GifFoo {
 					}
 				},
 				"lesJoiesdelEtudiantInfo",
-				context.getString(R.string.about)
+				context.getString(R.string.about),
+				Activity_Main.class
 		);
 	}
 }
--- app/src/main/java/com/chteuchteu/lesjoiesdeletudiantinfo/serv/NotificationService.java
+++ app/src/main/java/com/chteuchteu/lesjoiesdeletudiantinfo/serv/NotificationService.java
@@ -1,132 +1,41 @@
 package com.chteuchteu.lesjoiesdeletudiantinfo.serv;
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
-import android.os.AsyncTask;
 import android.os.IBinder;
 import android.os.PowerManager;
 import android.os.PowerManager.WakeLock;
-import android.support.v4.app.NotificationCompat;
 
-import com.chteuchteu.gifapplicationlibrary.GifApplicationSingleton;
-import com.chteuchteu.gifapplicationlibrary.hlpr.CacheUtil;
-import com.chteuchteu.gifapplicationlibrary.hlpr.MainUtil;
-import com.chteuchteu.gifapplicationlibrary.obj.Gif;
+import com.chteuchteu.gifapplicationlibrary.async.NotificationsPollTask;
 import com.chteuchteu.lesjoiesdeletudiantinfo.GifFoo;
-import com.chteuchteu.lesjoiesdeletudiantinfo.R;
-import com.chteuchteu.lesjoiesdeletudiantinfo.ui.Activity_Main;
-
-import java.util.List;
 
 public class NotificationService extends Service {
 	private WakeLock mWakeLock;
-	private Context context;
-	
+
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
 	}
-	
+
 	@SuppressWarnings("deprecation")
-	private void handleIntent(Intent intent) {
+	private void handleIntent() {
 		PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
 		mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "com.chteuchteu.lesjoiesdeletudiantinfo");
 		mWakeLock.acquire();
 
-		context = this;
-		
 		ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
 		if (!cm.getBackgroundDataSetting()) {
 			stopSelf();
 			return;
 		}
-		
-		// do the actual work, in a separate thread
-		new PollTask().execute();
-	}
-	
-	private class PollTask extends AsyncTask<Void, Void, Void> {
-		private int nbUnseenGifs = 0;
-		private List<Gif> l;
-		private GifApplicationSingleton gas;
 
-		public PollTask() {
-			this.gas = GifApplicationSingleton.create(context, GifFoo.getApplicationBundle(context));
-		}
-		
-		@Override
-		protected Void doInBackground(Void... params) {
-			l = this.gas.getBundle().getDataSourceParser().parseDataSource(
-					this.gas.getBundle().getDataSourceUrl()
-			);
-			
-			String lastUnseenGif = MainUtil.Prefs.getPref(context, "lastViewed");
-			if (l.size() > 0) {
-				for (Gif g : l) {
-					if (g.getArticleUrl().equals(lastUnseenGif))
-						break;
-					else
-						nbUnseenGifs++;
-				}
-			}
-			
-			if (nbUnseenGifs > 0)
-				CacheUtil.saveGifs(getApplicationContext(), l);
-			
-			return null;
-		}
-		
-		@Override
-		protected void onPostExecute(Void result) {
-			// Check if there are new gifs, and if a notification for them hasn't been displayed yet
-			boolean notif = (nbUnseenGifs > 0 && 
-					(MainUtil.Prefs.getPref(context, "lastNotifiedGif").equals("")
-							|| l.size() > 0 && !l.get(0).getGifUrl().equals(MainUtil.Prefs.getPref(context, "lastNotifiedGif"))));
-			
-			if (notif) {
-				// Save the last gif
-				if (l.size() > 0)
-					MainUtil.Prefs.setPref(context, "lastNotifiedGif", l.get(0).getGifUrl());
-				
-				String title = "Les Joies de l'Etudiant Info";
-				String text;
-				if (nbUnseenGifs > 1)
-					text = nbUnseenGifs + " nouveaux gifs !";
-				else
-					text = "1 nouveau gif !";
-				
-				NotificationCompat.Builder builder =
-						new NotificationCompat.Builder(NotificationService.this)
-				.setSmallIcon(R.drawable.ic_notifications)
-				.setNumber(nbUnseenGifs)
-				.setContentTitle(title)
-				.setAutoCancel(true)
-				.setContentText(text);
-				int NOTIFICATION_ID = 1664;
-				
-				Intent targetIntent = new Intent(NotificationService.this, Activity_Main.class);
-				PendingIntent contentIntent = PendingIntent.getActivity(NotificationService.this, 0, targetIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-				builder.setContentIntent(contentIntent);
-				NotificationManager nManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-				nManager.notify(NOTIFICATION_ID, builder.build());
-			}
-			stopSelf();
-		}
-	}
-	
-
-	@Override
-	public void onStart(Intent intent, int startId) {
-		handleIntent(intent);
+		new NotificationsPollTask(this, GifFoo.getApplicationBundle(this)).execute();
 	}
 	
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
-		handleIntent(intent);
+		handleIntent();
 		return START_NOT_STICKY;
 	}
 	
