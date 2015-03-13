94a1692dced7d39c975496548b94286ff6d8682c
Quentin S <chteuchteu@gmail.com>
2015-03-13 08:17:36 +0100

    Enabling or disabling notifications gets moved in GifApplicationLibrary

--- app/src/main/java/com/chteuchteu/lesjoiesdeletudiantinfo/GifFoo.java
+++ app/src/main/java/com/chteuchteu/lesjoiesdeletudiantinfo/GifFoo.java
@@ -6,6 +6,7 @@ import com.chteuchteu.gifapplicationlibrary.i.IDataSourceParser;
 import com.chteuchteu.gifapplicationlibrary.obj.Gif;
 import com.chteuchteu.gifapplicationlibrary.obj.GifApplicationBundle;
 import com.chteuchteu.lesjoiesdeletudiantinfo.hlpr.RSSReader;
+import com.chteuchteu.lesjoiesdeletudiantinfo.serv.NotificationService;
 import com.chteuchteu.lesjoiesdeletudiantinfo.ui.Activity_Main;
 
 import java.util.List;
@@ -23,7 +24,8 @@ public class GifFoo {
 				},
 				"lesJoiesdelEtudiantInfo",
 				context.getString(R.string.about),
-				Activity_Main.class
+				Activity_Main.class,
+                NotificationService.class
 		);
 	}
 }
