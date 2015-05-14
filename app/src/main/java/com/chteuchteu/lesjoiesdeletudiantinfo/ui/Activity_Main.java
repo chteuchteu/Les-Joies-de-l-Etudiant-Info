3034ca664ef97a15a39085a3c2a92c395fa3d86c
Quentin S <chteuchteu@gmail.com>
2015-05-14 17:04:47 +0200

    Include Crashlytics to project

--- app/src/main/java/com/chteuchteu/lesjoiesdeletudiantinfo/ui/Activity_Main.java
+++ app/src/main/java/com/chteuchteu/lesjoiesdeletudiantinfo/ui/Activity_Main.java
@@ -2,19 +2,22 @@ package com.chteuchteu.lesjoiesdeletudiantinfo.ui;
 
 import android.app.AlertDialog;
 import android.os.Bundle;
-import android.view.MenuItem;
 
 import com.chteuchteu.gifapplicationlibrary.GifApplicationSingleton;
 import com.chteuchteu.gifapplicationlibrary.ui.Super_Activity_Main;
 import com.chteuchteu.lesjoiesdeletudiantinfo.GifFoo;
 import com.chteuchteu.lesjoiesdeletudiantinfo.R;
+import com.crashlytics.android.Crashlytics;
 import com.tjeannin.apprate.AppRate;
+
+import io.fabric.sdk.android.Fabric;
 
 public class Activity_Main extends Super_Activity_Main {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		GifApplicationSingleton.create(this, GifFoo.getApplicationBundle(this));
 		super.onCreate(savedInstanceState);
+		Fabric.with(this, new Crashlytics());
 
 		AlertDialog.Builder builder = new AlertDialog.Builder(this)
 			.setTitle(getString(R.string.vote_title))
